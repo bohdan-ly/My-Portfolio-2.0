5a0341dce9c9719c0ce892a246f81d878d8d359f
TwelveGEater <dronkfc@gmail.com>
2020-04-30 00:02:12 +0300

    third commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,16 +9,6 @@ const cssBar = document.querySelector('.bar-css');
 const javascriptBar = document.querySelector('.bar-javascript');
 const reactBar = document.querySelector('.bar-react');
 const reduxBar = document.querySelector('.bar-redux');
-debugger;
-
-let statusBar = new TimelineLite();
-
-statusBar
-	.fromTo(htmlBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(75% - 6px)`, easy: Power4.easyOut })
-	.fromTo(cssBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(50% - 6px)`, easy: Power4.easyOut })
-	.fromTo(javascriptBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(80% - 6px)`, easy: Power4.easyOut })
-	.fromTo(reactBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(40% - 6px)`, easy: Power4.easyOut })
-	.fromTo(reduxBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(30% - 6px)`, easy: Power4.easyOut });
 
 let showMenu = false;
 
@@ -40,3 +30,12 @@ function toggleMenu() {
 		showMenu = false;
 	}
 }
+
+let statusBar = new TimelineLite();
+
+statusBar
+	.fromTo(htmlBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(75% - 6px)`, easy: Power4.easyOut })
+	.fromTo(cssBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(50% - 6px)`, easy: Power4.easyOut })
+	.fromTo(javascriptBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(80% - 6px)`, easy: Power4.easyOut })
+	.fromTo(reactBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(40% - 6px)`, easy: Power4.easyOut })
+	.fromTo(reduxBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(30% - 6px)`, easy: Power4.easyOut });

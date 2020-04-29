07d8a939ef1c5565f043c58f77f7c1380623c04e
TwelveGEater <dronkfc@gmail.com>
2020-04-29 22:35:18 +0300

    second commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,22 @@ const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav__item');
 
+const htmlBar = document.querySelector('.bar-html');
+const cssBar = document.querySelector('.bar-css');
+const javascriptBar = document.querySelector('.bar-javascript');
+const reactBar = document.querySelector('.bar-react');
+const reduxBar = document.querySelector('.bar-redux');
+debugger;
+
+let statusBar = new TimelineLite();
+
+statusBar
+	.fromTo(htmlBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(75% - 6px)`, easy: Power4.easyOut })
+	.fromTo(cssBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(50% - 6px)`, easy: Power4.easyOut })
+	.fromTo(javascriptBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(80% - 6px)`, easy: Power4.easyOut })
+	.fromTo(reactBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(40% - 6px)`, easy: Power4.easyOut })
+	.fromTo(reduxBar, 0.75, { width: `calc(0% - 6px)` }, { width: `calc(30% - 6px)`, easy: Power4.easyOut });
+
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
